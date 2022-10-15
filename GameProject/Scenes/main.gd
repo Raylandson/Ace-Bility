fd7abc76a6fa778027a0fd4fc21fdc7998f01dd7
raylandson <www.raylandsoncs@gmail.com>
2022-10-15 14:01:44 -0300

    wheelchair fixed and beta pause

--- GameProject/Scenes/main.gd
+++ GameProject/Scenes/main.gd
@@ -1,8 +1,18 @@
 extends Node2D
 
+var paused = true
 
 func _ready():
 	pass 
 
 func _process(delta):
 	pass
+
+
+func _input(event):
+	if event.is_action_pressed('pause'):
+		paused = !paused
+		Autoload.current_state = int(paused)
+#		get_tree().paused = !get_tree().paused
+#		print(Autoload.current_state)
+
