90682b49921dd82ace46dd6658e7c75bbb9455ce
raylandson <www.raylandsoncs@gmail.com>
2022-10-15 19:56:02 -0300

    movement updated again

--- GameProject/Scenes/player/player.gd
+++ GameProject/Scenes/player/player.gd
@@ -18,26 +18,27 @@ func _physics_process(delta):
 	
 	
 	_velocity = move_and_slide_with_snap(_velocity.rotated(rotation), transform.y * 1, -transform.y,  true, 4, PI/3)
+#	_velocity = _velocity.rotated(-rotation)
 	if is_on_floor():
-		_velocity = _velocity.rotated(-rotation)
 #		rotation = get_floor_normal().angle() + PI/2
 		var floor_angle = get_floor_normal().angle() + PI/2
-		if rotation != floor_angle:
+		if $Sprite.rotation != floor_angle:
+#			rotation.linear_interpolate()
 			if $RightRay.is_colliding() and sign(_velocity.x) == 1:
-				rotation = max((abs(rotation) + PI/smooth_climb) * sign(get_floor_normal().angle()), floor_angle)
+				$Sprite.rotation = max((abs($Sprite.rotation) + PI/smooth_climb) * sign(get_floor_normal().angle()), floor_angle)
 			elif $LeftRay.is_colliding() and sign(_velocity.x) == -1:
 #				printt(rotation, rotation + PI/smooth_climb * sign(floor_angle), floor_angle)
-				rotation = min(rotation + PI/smooth_climb , floor_angle)
-			elif is_equal_approx(0, floor_angle):
-				rotation = max((abs(rotation) + PI/smooth_climb) * sign(get_floor_normal().angle()), floor_angle)
+				$Sprite.rotation = min($Sprite.rotation + PI/smooth_climb , floor_angle)
+#			elif is_equal_approx(0, floor_angle):
+#				rotation = max((abs(rotation) + PI/smooth_climb) * sign(get_floor_normal().angle()), floor_angle)
 				
 #rotation = min(rotation, rota)
 #			prints(rotation, rotation + PI/10, get_floor_normal().angle() + PI/2)
-	else:
+	elif not $LeftRay.is_colliding():
 		if sign(_velocity.x) == 1:
-			rotation = min(0, rotation + PI/smooth_fall * sign(_velocity.x))
+			$Sprite.rotation = min(0, $Sprite.rotation + PI/smooth_fall * sign(_velocity.x))
 		else:
-			rotation = max(0, rotation + PI/smooth_fall * sign(_velocity.x))
+			$Sprite.rotation = max(0, $Sprite.rotation + PI/smooth_fall * sign(_velocity.x))
 
 
 func _process(delta):
